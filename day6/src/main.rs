6c28a2d91bcf86ac8b42edd94f108f8c8cdffde1
ArcticPheenix <phoenix.ckelly@gmail.com>
2020-12-31 20:13:03 -0700

    WIP: Making notes on what to do next.

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    // Read input file
+    let filename = "./input";
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    // Grab record lines from input file and store them in a Vector.
+    let mut input_vec = Vec::new();
+    for line in reader.lines() {
+        let line = line.unwrap();
+        input_vec.push(line);
+    }
+
+    // TODO - Process the input file.
+    //        Each "group" is separated by an empty line.
+    //        Iterate on the characters for each line, adding the characters to a HashSet (to ensure uniqueness).
+    //        When a new line is encountered, add the set to a Vector and then create a new set for the next group.
+    //        After processing is complete, output the total number of "yes" answers there are to STDOUT.
 }
